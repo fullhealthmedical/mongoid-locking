c070b545747ef37abe6265d72d203b9c22236de5
Rodrigo Ra <rodrigorcomp@gmail.com>
2022-08-31 15:41:25 -0300

    init

---
+++ lib/mongoid/locking.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "locking/version"
+
+module Mongoid
+  module Locking
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
